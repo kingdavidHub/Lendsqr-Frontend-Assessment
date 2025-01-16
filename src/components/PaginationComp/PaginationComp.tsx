ffad9a1782e6f199c5482012314f8036b78c61b1
David Igbayilola <kingigbayilola@gmail.com>
2025-01-16 21:04:13 +0100

    feat: enhance Navbar and Pagination components with improved styling and functionality; update select input and add icon for better user experience

--- src/components/PaginationComp/PaginationComp.tsx
+++ src/components/PaginationComp/PaginationComp.tsx
@@ -1,8 +1,8 @@
 import classNames from "classnames";
-import styles from "./PaginationComp.module.scss"
+import styles from "./PaginationComp.module.scss";
 import { PaginationProps } from "../../types";
 import ReactPaginate from "react-paginate";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
 const PaginationComp = ({
   setCurrentRange,
   setCurrentPage,
@@ -13,47 +13,56 @@ const PaginationComp = ({
   const handlePageClick = (data: { selected: number }) => {
     setCurrentPage(data.selected + 1);
   };
+
+  //Todo: provide a better UI for global ErrorBoundary
+  // const defaultRange = [0, 1, 2, 3, 4];
+  // // [100, 200, 300, 400, 500]
+  // defaultRae.map((_, index) => {
+  //   console.log(++index * 100);
+  // });
   return (
     <div className={classNames(styles.pagination, "padding-1")}>
       <div className="flexCenter gap-1">
         <span>Showing</span>
-        <select
-          id="rangeSelect"
-          className={styles.perPage}
-          onChange={(e) => {
-            setCurrentRange(ranges[parseInt(e.target.value)]);
-            setCurrentPage(1);
-          }}
-        >
-          {ranges.map((_, index) => (
-            <option key={index} value={index}>
-              {`${index * 100}-${index * 100 + 99}`}
-            </option>
-          ))}
-        </select>
+        <div className={styles.selectContainer}>
+          <select
+            id="rangeSelect"
+            className={styles.select}
+            onChange={(e) => {
+              setCurrentRange(ranges[parseInt(e.target.value)]);
+              setCurrentPage(1);
+            }}
+          >
+            {Array.from({ length: 4 }).map((_, index) => (
+              <option key={index} value={index}>
+                {`${++index * 100}`}
+              </option>
+            ))}
+          </select>
+          <ChevronDown className={styles.icon} color="#213F7D"/>
+        </div>
 
-        <span>out of 100</span>
+        <span>out of {ranges.flat().length}</span>
       </div>
-        <ReactPaginate
-          previousLabel={<ChevronLeft color="#213F7D"/>}
-          previousLinkClassName={styles.prev}
-          nextLabel={<ChevronRight color="#213F7D"/>}
-          nextLinkClassName={styles.next}
-          breakLabel={"..."}
-          pageCount={totalPages}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={1}
-          onPageChange={handlePageClick}
-          containerClassName={styles.pageNumbers}
-          activeClassName={styles.active}
-          pageClassName={styles.page}
-          previousClassName={styles.prev}
-          nextClassName={styles.next}
-          breakClassName={styles.break}
-        />
+      <ReactPaginate
+        previousLabel={<ChevronLeft color="#213F7D" />}
+        previousLinkClassName={styles.prev}
+        nextLabel={<ChevronRight color="#213F7D" />}
+        nextLinkClassName={styles.next}
+        breakLabel={"..."}
+        pageCount={totalPages}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={1}
+        onPageChange={handlePageClick}
+        containerClassName={styles.pageNumbers}
+        activeClassName={styles.active}
+        pageClassName={styles.page}
+        previousClassName={styles.prev}
+        nextClassName={styles.next}
+        breakClassName={styles.break}
+      />
     </div>
   );
 };
-
 
 export default PaginationComp;
